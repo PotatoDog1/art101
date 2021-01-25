abfc09356062af94e530d1ae26138c466c0647d2
PotatoDog1 <gwyu@ucsc.edu>
2021-01-25 15:47:31 -0800

    lab 4 changes

---
+++ lab4/js/lab.js
@@ -0,0 +1,26 @@
+// Players choose white/black pieces
+  // First player picks color of pieces (black or white)
+  // Automatically assign second player with the other color
+// Print the board with pieces in default spaces.
+  // Loop through each row
+    // Loop through each space
+      // Print either a piece or a space
+      // Print a | in between
+    // Print a newline at the end
+    // Print a horizontal line between
+// White moves first
+// Print the board before each turn.
+  // Loop through each row
+    // Loop through each space
+      // Print either a piece or a space
+      // Print a | in between
+    // Print a newline at the end
+    // Print a horizontal line between
+// Black evaluates possible moves.
+// Accept user input.
+  // Prompt user to choose chess piece to move
+// Validate user move.
+  // Check player input against possible moves
+// Black evaluates possible moves
+// Black chooses a move
+// Test for Win! Condition.
