85a9f681aa9ae09aed808b21c926fa0cb601875d
PotatoDog1 <gwyu@ucsc.edu>
2021-03-04 01:52:34 -0800

    lab 15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -30,7 +30,7 @@ function putTextOnPage(text){
 //      xhr.send();
 // }
 
-
+//
 function doAPIStuff() {
   console.log("doing api stuff");
   $.ajax({
@@ -64,3 +64,43 @@ function doAPIStuff() {
     }
   })
 }
+
+// function doAPIStuff() {
+//   console.log("doing api stuff");
+//   $.ajax({
+//     // The URL for the request (from the api docs)
+//     url: "https://api.nasa.gov/planetary/apod",
+//     // The data to send (will be converted to a query string)
+//     data: {
+//       api_key: "YdheUYkCQCutDdoisfF9oAjtiDymR1zZtYi18NmR",
+//     },
+//     // Whether this is a POST or GET request
+//     type: "GET",
+//     // The type of data we expect back
+//     dataType : "json",
+//     // What do we do when the api call is successful
+//     // all the action goes in here
+//     success: function(data) {
+//       // do stuff
+//       var textData = JSON.stringify(data);
+//       var str = "";
+//       var imageURL = data.hdurl;
+//       var date = data.date;
+//       var explanation = data.explanation;
+//       var copyright = data.copyright;
+//       str += "<h2>" + date + "</h2>";
+//       str += "<img src='" + imageURL + "'>";
+//       str += explanation;
+//       str += "<br>Copyright: " + copyright;
+//       console.log("Success!", textData);
+//       console.log(data);
+//       putTextOnPage(str);
+//     },
+//     // What we do if the api call fails
+//     error: function (jqXHR, textStatus, errorThrown) {
+//       // do stuff
+//       console.log("Error:", textStatus, errorThrown);
+//       putTextOnPage(errorThrown + " Status:" + status);
+//     }
+//   })
+// }
